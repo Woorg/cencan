611eb143e458b1a35d1688bd72bc762fc4c655b1
Igor Gorlov <gorlov35@gmail.com>
2019-10-23 09:10:13 +0300

    add adaptive

--- markup/assets/js/main.js
+++ markup/assets/js/main.js
@@ -15,51 +15,62 @@ import svg4everybody from 'svg4everybody';
 
 (function ($) {
 
-    svg4everybody();
+  svg4everybody();
 
-    let styles = [
-        'padding: 2px 9px',
-        'background: #82B93C',
-        'color: #fff',
-        'display: inline-block',
-        'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.2)',
-        'box-shadow: 0 -1px 0 rgba(255, 255, 255, 0.2) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset',
-        'line-height: 1.56',
-        'text-align: left',
-        'font-size: 16px',
-        'font-weight: 400'
-    ].join(';');
+  let styles = [
+    'padding: 2px 9px',
+    'background: #82B93C',
+    'color: #fff',
+    'display: inline-block',
+    'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.2)',
+    'box-shadow: 0 -1px 0 rgba(255, 255, 255, 0.2) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset',
+    'line-height: 1.56',
+    'text-align: left',
+    'font-size: 16px',
+      'font-weight: 400'
+  ].join(';');
 
-    console.log('%c developed by igor gorlov gorlov https://webjeb.ru', styles);
-
-
-     /* 
-        Lazyload images
-      */
+  console.log('%c developed by igor gorlov gorlov https://webjeb.ru', styles);
 
 
-    let lazyLoadInstance = new LazyLoad({
-        elements_selector: '.lazy',
-        load_delay: 50
-        // ... more custom settings?
+   /* 
+      Lazyload images
+    */
+
+
+  let lazyLoadInstance = new LazyLoad({
+    elements_selector: '.lazy',
+    load_delay: 50
+    // ... more custom settings?
+  });
+
+
+  if (lazyLoadInstance) {
+    lazyLoadInstance.update();
+  }
+
+
+  // var webpMachine = new webpHero.WebpMachine();
+  // webpMachine.polyfillDocument();
+
+
+  $(function() {
+
+    const $navTrigger = $('.nav__trigger');
+
+    $navTrigger.on('click', function (e) {
+      e.preventDefault();
+      $(this).toggleClass('nav__trigger_active');
     });
 
 
-    if (lazyLoadInstance) {
-        lazyLoadInstance.update();
-    }
-
-
-    // var webpMachine = new webpHero.WebpMachine();
-    // webpMachine.polyfillDocument();
-
-
-
-    $(function() {
-
-      
-
+    $(document).on('click', function(e) {
+      if (!$(e.target).closest('.nav__trigger_active').length) {
+          $navTrigger.removeClass('nav__trigger_active');
+      }
     });
+
+  });
 
 
     
